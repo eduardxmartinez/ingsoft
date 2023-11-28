b8cc355859a3afb65632499c324976e7061f2433
eduardxmartinez <eduardom17809@gmail.com>
2023-11-28 12:18:10 -0700

    logo update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
-import Homepage from './homepage';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <Homepage/>
+    <Navbar/>
   );
 }
 
---
+++ src/Navbar/index.js
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.png"
+
+function Navbar() {
+    return(
+        <img src={logo} alt="logo"/>
+    )
+}
+
+export default Navbar;
